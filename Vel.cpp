be8cfe0f463ca3025d8c456875cb82c7adb12ac3
王修奇 <111209848+Animex77@users.noreply.github.com>
2022-11-13 11:37:05 +0800

    Add files via upload

--- Vel.cpp
+++ Vel.cpp
@@ -5,7 +5,7 @@
 
 void VelCallback(const turtlesim::Pose::ConstPtr& msg)
 {
-  ROS_INFO("Vel: linear_velocity:%0.6f, angular_velocity:%0.6f", msg->linear_velocity,msg->angular_velocity);
+  ROS_INFO("Vel: linear_velocity:%0.6f, angular_velocity:%0.6f, theta:%0.6f", msg->linear_velocity, msg->angular_velocity, msg->theta);
 }
 
 int main(int argc, char **argv)
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
   
   ros::NodeHandle n;
 
-  ros::Subscriber pose_sub = n.subscribe("turtlesim/Pose.h", 10, VelCallback);
+  ros::Subscriber pose_sub = n.subscribe("/turtle1/pose", 10, VelCallback);
 
 
   ros::spin();
